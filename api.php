527b417dc47013acba6c30877145e001ae3b6262
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-05-28 18:29:31 -0500

    feat: :sparkles: Fifth Exercise
    determine the date and time one gigasecond after a certain date.

--- api.php
+++ api.php
@@ -1,30 +1,18 @@
 <?php
 
 declare(strict_types=1);
-$strandA = 'GAGCCTACTAACGGGAT';
-$strandB = 'CATCGTAATGACGGCCT';
-function distance(string $strandA, string $strandB): int
+
+function from(DateTimeImmutable $date): DateTimeImmutable
 {
-    $distance = 0;
-    if (strlen($strandA) !== strlen($strandB)) {
-        throw new InvalidArgumentException('DNA strands must be of equal length.');
-    }
+    $gigasecond = new DateInterval('PT1000000000S'); // Crea un intervalo de un gigasegundo
+    $newDate = $date->add($gigasecond); // Agrega el intervalo a la fecha proporcionada
 
-    // return count(
-    //     array_diff_assoc(
-    //        str_split($strandA), 
-    //         str_split($strandB)
-    //     )
-    // ); 
+    return $newDate;
+}
+$birthdate = new DateTimeImmutable('2002-04-13 22:00:00');
+$newDateTime = from($birthdate);
 
-    for ($i=0; $i < strlen($strandA) ; $i++) { 
-        if ($strandA[$i] !== $strandB[$i]) {
-            $distance++;
-        }
-    };
-    return $distance;
-}; 
-
-echo distance($strandA, $strandB);
+echo 'Fecha y hora de nacimiento: ' . $birthdate->format('Y-m-d H:i:s') . PHP_EOL;
+echo 'Fecha y hora después de un gigasegundo: ' . $newDateTime->format('Y-m-d H:i:s');
 
 ?>
