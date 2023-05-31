75e2baeee1baea7660734f91d8ebb54a08af4b66
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-05-31 13:45:57 -0500

    feat: :sparkles: Eighth Exercise
    write methods that return the highest score from the list, the last added score and the three highest scores.

--- api.php
+++ api.php
@@ -2,59 +2,19 @@
 
 declare(strict_types=1);
 
-class SimpleCipher
+class HighScores
 {
-    public $key;
-    public function __construct(string $key = null)
+    
+    public function __construct(array $scores)
     {
-        if($key === ''){
-            throw new InvalidArgumentException('Key must not be empty');
-        }
-        $key = $key ?? $this->getRandomKey();
-        if (!ctype_lower($key)){
-            throw new InvalidArgumentException('Key must be lowercase alphabets only');
-        }
-        $this->key = $key;
-    }
+        $this->scores = $scores;
+        $this->latest = end($scores);
+        $this->personalBest = max($scores);
 
-    private function getRandomKey(): string
-    {
-        $chars = range('a','z');
-        $key = '';
+        rsort($scores);
 
-        for ($i=0; $i<100; $i++){
-            $key .= $chars[random_int(0,25)];
-        }
-        return $key;
-    }
-
-    public function encode(string $plainText): string
-    {
-        $encoded = '';
-        $keyLen = strlen($this->key);
-
-        foreach (str_split($plainText) as $i => $operation) {
-            $key = ord($this->key[$i % $keyLen]) - ord('a');
-            $operation = (ord($operation) - ord('a') + $key) % 26 + ord('a');
-            $encoded .= chr($operation);
-        }
-        return $encoded;
-    }
-
-    public function decode(string $cipherText): string
-    {
-        $decoded = '';
-        $keyLen = strlen($this->key);
-
-        foreach (str_split($cipherText) as $i => $operation) {
-            $key = ord($this->key[$i % $keyLen]) - ord('a');
-            $operation = (ord($operation) - ord('a') - $key + 26) % 26 + ord('a');
-            $decoded .= chr($operation);
-        }
-
-        return $decoded;
+        $this->personalTopThree = array_slice($scores, 0, 3);
     }
 }
-
 
 ?>
