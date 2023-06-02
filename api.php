1ab724c2632a3d93b1faad242678d1103b219e74
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-06-02 13:14:11 -0500

    feat: :sparkles: Tenth Exercise
    RNA Transcription

--- api.php
+++ api.php
@@ -1,26 +1,27 @@
 <?php
 
-
 declare(strict_types=1);
-class Bob
-{
-    public function respondTo(string $str): string
-    {
-    $str=trim($str);
-    if(empty($str)){
-        return "Fine. Be that way!";
-    }
-    if (preg_match('/[A-Z]+/', $str) && !preg_match('/[a-z]+/', $str)) { 
 
-            if (str_ends_with($str, '?')) {
-                return 'Calm down, I know what I\'m doing!';
-            }
-            return 'Whoa, chill out!';
-        }
-    if (str_ends_with($str, '?')) {
-            return 'Sure.';
-        }
-        return 'Whatever.';
-    }
+function toRna(string $dna): string
+{
+    return strtr($dna,['G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U']);
 }
+
+
+/* if($dna == "G"){
+    $dna = str_replace("G", "C", $dna);
+    return $dna;
+}
+else if($dna == "C"){
+    $dna = str_replace("C", "G", $dna);
+    return $dna;
+}
+else if($dna == "T"){
+    $dna = str_replace("T", "A", $dna);
+    return $dna;
+}
+else if($dna == "A"){
+    $dna = str_replace("A", "U", $dna);
+    return $dna;
+} */
 ?>
