cfac5c76c636586ae7549280d5cd1f0897332acd
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-06-06 15:14:30 -0500

    feat: :sparkles: Twelveth Exercise
    Isogram

--- api.php
+++ api.php
@@ -2,33 +2,24 @@
 
 declare(strict_types=1);
 
-function isValid(string $number): bool
+function isIsogram(string $word): bool
 {
-    $number = str_replace(' ', '', $number);
-    if (strlen($number) < 2 or !ctype_digit($number)){
-        return false;
-    }
-    if(doubling($number)) {
-        return false;
-    }
-    return true;
+$data = mb_str_split(preg_replace('/[\s-]/', '', mb_strtolower($word)));
+return array_unique($data) === $data;
 }
-function doubling(string $number)
+
+
+/* function isIsogram(string $word): bool
 {
-    $length = strlen($number);
-    $even = $length % 2;
-    $sum = 0;
-    for ($i = $length - 1; $i >= 0; --$i) {
-        $char = $number[$i];
-        if ($i % 2 === $even) {
-            $char *= 2;
-            if ($char > 9) {
-                $char -= 9;
-            }
-        }
-        $sum += $char;
-    }
-    return ($sum * 9) % 10;
+$word = str_replace(' ','',str_replace('-','',strtolower($word)));
+$len = array();
+foreach(str_split($word) as $letter){
+if(in_array($letter, $len)){
+return false;
 }
+$len[] = $letter;
+}
+return true;
+} */
 
 ?>
