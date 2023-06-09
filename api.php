ecdf06f57a5fe906b36c690fa2939aeb593ec605
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-06-09 07:38:23 -0500

    feat: :sparkles: Sixteenth Exercise
    Robot Simulator

--- api.php
+++ api.php
@@ -2,22 +2,47 @@
 
 declare(strict_types=1);
 
-class School
+class Robot
 {
-    private $school = [];
-    public function add(string $name, int $grade): void
+    public const DIRECTION_NORTH = 0;
+    public const DIRECTION_EAST = 1;
+    public const DIRECTION_SOUTH = 2;
+    public const DIRECTION_WEST = 3;
+    public function __construct(public array $position, public int $direction)
     {
-        $this->school[$grade][] = $name;
-        sort($this->school[$grade]);
     }
-    public function grade(int $grade): array
+    public function turnRight(): self
     {
-        return $this->school[$grade] ?? [];
+        $this->direction = ($this->direction + 1) % 4;
+        return $this;
     }
-    public function studentsBygradeAlphabetical(): array
+    public function turnLeft(): self
     {
-        ksort($this->school);
-        return $this->school;
+        $this->direction = ($this->direction + 3) % 4;
+        return $this;
+    }
+    public function advance(): self
+    {
+        match ($this->direction) {
+            self::DIRECTION_NORTH => $this->position[1]++,
+            self::DIRECTION_EAST => $this->position[0]++,
+            self::DIRECTION_SOUTH => $this->position[1]--,
+            self::DIRECTION_WEST => $this->position[0]--
+        };
+        return $this;
+    }
+    public function instructions(string $instructions): void
+    {
+        if (!preg_match('/^[LRA]+$/', $instructions)) {
+            throw new InvalidArgumentException();
+        }
+        foreach (str_split($instructions) as $instruction) {
+            match ($instruction) {
+                'L' => $this->turnLeft(),
+                'R' => $this->turnRight(),
+                'A' => $this->advance()
+            };
+        }
     }
 }
 
