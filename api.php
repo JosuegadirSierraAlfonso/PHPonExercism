8cfc25e8b351f2c0044f1196fb5fd1efab56cc51
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-06-11 09:14:40 -0500

    feat: :sparkles: Seventeenth Exercise
    Run-Length Encoding

--- api.php
+++ api.php
@@ -2,48 +2,28 @@
 
 declare(strict_types=1);
 
-class Robot
+
+function encode(string $input): string
 {
-    public const DIRECTION_NORTH = 0;
-    public const DIRECTION_EAST = 1;
-    public const DIRECTION_SOUTH = 2;
-    public const DIRECTION_WEST = 3;
-    public function __construct(public array $position, public int $direction)
-    {
-    }
-    public function turnRight(): self
-    {
-        $this->direction = ($this->direction + 1) % 4;
-        return $this;
-    }
-    public function turnLeft(): self
-    {
-        $this->direction = ($this->direction + 3) % 4;
-        return $this;
-    }
-    public function advance(): self
-    {
-        match ($this->direction) {
-            self::DIRECTION_NORTH => $this->position[1]++,
-            self::DIRECTION_EAST => $this->position[0]++,
-            self::DIRECTION_SOUTH => $this->position[1]--,
-            self::DIRECTION_WEST => $this->position[0]--
-        };
-        return $this;
-    }
-    public function instructions(string $instructions): void
-    {
-        if (!preg_match('/^[LRA]+$/', $instructions)) {
-            throw new InvalidArgumentException();
-        }
-        foreach (str_split($instructions) as $instruction) {
-            match ($instruction) {
-                'L' => $this->turnLeft(),
-                'R' => $this->turnRight(),
-                'A' => $this->advance()
-            };
-        }
-    }
+    preg_match_all('/(.)\1*/', $input, $matches);
+    return implode(
+        array_map(
+            fn($count, $value) => ($count === 1 ? "" : strval($count)) . $value,
+            array_map(fn($match) => strlen($match), $matches[0]),
+            $matches[1]
+        )
+    );
+}
+function decode(string $input): string
+{
+    preg_match_all('/(\d*)(\D)/', $input, $matches);
+    return implode(
+        array_map(
+            fn($count, $value) => str_repeat($value, $count),
+            array_map(fn($match) => empty($match) ? 1 : intval($match), $matches[1]),
+            $matches[2]
+        )
+    );
 }
 
 
