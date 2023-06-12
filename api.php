195219c0ef0c5b6df8a681c42b09815f6ac6dd38
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-06-12 08:40:57 -0500

    feat: :sparkles: Exercise Twenty-five
    Binary

--- api.php
+++ api.php
@@ -2,38 +2,18 @@
 
 declare(strict_types=1);
 
-class BeerSong
+function parse_binary(string $binary): int
 {
-    public function lyrics(): string
-    {
-        return $this->verses(99, 0);
-    }
-    public function verse(int $number): string
-    {
-        switch ($number) {
-            case 1:
-                return
-                    '1 bottle of beer on the wall, 1 bottle of beer.' . PHP_EOL .
-                    'Take it down and pass it around, no more bottles of beer on the wall.' . PHP_EOL;
-            case 0:
-                return
-                    'No more bottles of beer on the wall, no more bottles of beer.' . PHP_EOL .
-                    'Go to the store and buy some more, 99 bottles of beer on the wall.';
-            default:
-                $next = $number - 1;
-                $noun = ($next === 1) ? 'bottle' : 'bottles';
-                return
-                    "{$number} bottles of beer on the wall, {$number} bottles of beer." . PHP_EOL .
-                    "Take one down and pass it around, {$next} {$noun} of beer on the wall." . PHP_EOL;
+    $decimal = 0;
+    $binaryArray = str_split($binary);
+    $binaryArray = array_reverse($binaryArray);
+    foreach ($binaryArray as $key => $char) {
+        if ($char != '0' && $char != '1') {
+            throw new \InvalidArgumentException('');
         }
+        $decimal += (intval($char)) * pow(2, $key);
     }
-    public function verses(int $from, int $to): string
-    {
-        return implode(PHP_EOL, array_map(
-            fn($number) => $this->verse($number),
-            range($from, $to)
-        ));
-    }
+    return $decimal;
 }
 
 ?>
