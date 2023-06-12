7b394a7fca79b5ad2b6be417c10b04bc5d70d3ab
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-06-11 21:21:28 -0500

    feat: :sparkles: Exercise Twenty-three
    Atbash Cipher

--- api.php
+++ api.php
@@ -2,24 +2,17 @@
 
 declare(strict_types=1);
 
-
-function detectAnagrams(string $word, array $anagrams): array
+function encode(string $text): string
 {
-    $result = [];
-    $lowerWord = strtolower($word);
-    foreach ($anagrams as $anagram){
-        $lowerAnagram = strtolower($anagram);
-        if (strlen($anagram) !== strlen($word)){
-            continue;
-        }
-        if($lowerAnagram === $lowerWord){
-            continue;
-        }
-        if (count_chars($lowerAnagram) == count_chars ($lowerWord)){
-            array_push($result, $anagram);
-        }
-    }
-    return $result;
+    $alphabet = implode('', range('a', 'z'));
+
+    $ciphers = strrev($alphabet);
+
+    $text = preg_replace('/\W/', '', strtolower($text));
+
+    $translation = strtr($text, $alphabet, $ciphers);
+    
+    return wordwrap($translation, 5, ' ', true);
 }
 
 
