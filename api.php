91d113f0083cf145b4389f7bac6f3318259254a5
JosuegadirSierraAlfonso <josuegsierraa18@gmail.com>
2023-06-11 22:01:25 -0500

    feat: :sparkles: Exercise Twenty-four
    Beer song

--- api.php
+++ api.php
@@ -2,19 +2,38 @@
 
 declare(strict_types=1);
 
-function encode(string $text): string
+class BeerSong
 {
-    $alphabet = implode('', range('a', 'z'));
-
-    $ciphers = strrev($alphabet);
-
-    $text = preg_replace('/\W/', '', strtolower($text));
-
-    $translation = strtr($text, $alphabet, $ciphers);
-    
-    return wordwrap($translation, 5, ' ', true);
+    public function lyrics(): string
+    {
+        return $this->verses(99, 0);
+    }
+    public function verse(int $number): string
+    {
+        switch ($number) {
+            case 1:
+                return
+                    '1 bottle of beer on the wall, 1 bottle of beer.' . PHP_EOL .
+                    'Take it down and pass it around, no more bottles of beer on the wall.' . PHP_EOL;
+            case 0:
+                return
+                    'No more bottles of beer on the wall, no more bottles of beer.' . PHP_EOL .
+                    'Go to the store and buy some more, 99 bottles of beer on the wall.';
+            default:
+                $next = $number - 1;
+                $noun = ($next === 1) ? 'bottle' : 'bottles';
+                return
+                    "{$number} bottles of beer on the wall, {$number} bottles of beer." . PHP_EOL .
+                    "Take one down and pass it around, {$next} {$noun} of beer on the wall." . PHP_EOL;
+        }
+    }
+    public function verses(int $from, int $to): string
+    {
+        return implode(PHP_EOL, array_map(
+            fn($number) => $this->verse($number),
+            range($from, $to)
+        ));
+    }
 }
-
-
 
 ?>
